5662b23fad6f3574abf02f5f8d496f56ea8b6b81
aryopratamaa <mr.aryopratama@gmail.com>
2026-06-23 14:56:49 +0700

    14.56

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\DasborController;
+use app\Http\Controllers\TipeLayananController;
+use app\Http\Controllers\BengkelController;
+use app\Http\Controllers\UserController;
+use app\Http\Controllers\PromoController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get("/", [DasborController::class,"index"])->name('dashboard');
+
+Route::resource('tipelayanan', TipeLayananController::class);
+Route::resource('bengkels', BengkelController::class);
+Route::resource('users', UserController::class);
+Route::resource('promos', PromoController::class);
